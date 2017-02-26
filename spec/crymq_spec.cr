ae2f5624b9d436552c49eaedc015a133bf0b9979
raviteja <raviteja@ravitejas-MacBook-Air.local>
2017-02-27 01:39:32 +0530

    more packet write implementations and unit tests

--- spec/crymq_spec.cr
+++ spec/crymq_spec.cr
@@ -1,9 +1,22 @@
 require "./spec_helper"
 
 describe Crymq do
-  # TODO: Write tests
-
-  it "works" do
-    false.should eq(true)
+  it "connect packet write works" do
+    socket = IO::Memory.new
+    connect = Connect.new("hello", 10_u16)
+    socket.write_bytes(connect, IO::ByteFormat::NetworkEndian)
+    socket.to_slice.hexstring.should eq("101100044d5154540402000a000568656c6c6f")
+  end
+  it "connack packet write works" do
+    socket = IO::Memory.new
+    connack = Connack.new(true, 0_u8)
+    socket.write_bytes(connack, IO::ByteFormat::NetworkEndian)
+    socket.to_slice.hexstring.should eq("20020100")
+  end
+  it "publish packet write works" do
+    socket = IO::Memory.new
+    connack = Publish.new("hello/world", QoS::AtleastOnce, "hello world".to_slice, Pkid.new(100_u16))
+    socket.write_bytes(connack, IO::ByteFormat::NetworkEndian)
+    socket.to_slice.hexstring.should eq("321a000b68656c6c6f2f776f726c64006468656c6c6f20776f726c64")
   end
 end
--- src/crymq/packet.cr
+++ src/crymq/packet.cr
@@ -34,6 +34,10 @@ struct Pkid
     def initialize(@pkid)
     end
 
+    def value
+      @pkid
+    end
+
     def reset
         @pkid = 0
     end
@@ -160,17 +164,71 @@ struct Connect < Control
     end
 end
 
-struct Connack
+
+## Fixed header for CONNACK PACKET
+##
+## 7                          3                          0
+## +--------------------------+--------------------------+
+## |     CONNACK (2) NIBBLE   |     RESERVED             |   0
+## +--------------------------+--------------------------+
+## |    Remaining Len = Len of Varable header(2)         |   1
+## +-----------------------------------------------------+
+## 
+##
+## Variable header ( LENGTH = 2 Bytes)
+##
+## +--------------------------+--------------------------+
+## |   Reserved bits 1-7 must be set to 0 (0b0000 0000x) |   2
+## |   Bit 0 is 'session present' flag                   |
+## +-----------------------------------------------------+
+## +--------------------------+--------------------------+
+## |                  Connect Return Code                |   3
+## +-----------------------------------------------------
+struct Connack < Control
     @session_present : Bool
     @return_code : UInt8
 
-    def initialize
-        @session_present = false
-        @return_code = 0_u8
+    def initialize(@session_present, @return_code)
+    end
+
+    def to_io(io : IO, format : IO::ByteFormat = IO::ByteFormat::SystemEndian)
+      frame = UInt8.slice(0x20, 0x02, @session_present ? 1_u8:0_u8, @return_code.to_u8)
+      io.write(frame)
     end
 end
 
-struct Publish
+## Fixed header for PUBLISH PACKET
+##
+## 7                          3                          0
+## +--------------------------+--------------------------+
+## |     PUBLISH (3) NIBBLE   | DUP(1), QoS(2), Retain(1)|   0
+## +--------------------------+--------------------------+
+## | Remaining Len = Len of Variable header(10) + Payload|   1
+## +-----------------------------------------------------+
+## 
+##
+## Variable header ( LENGTH = 2 Bytes)
+##
+## +--------------------------+--------------------------+
+## |            TOPIC Name Length MSB (VALUE = 0)        |   2
+## +-----------------------------------------------------+
+## +--------------------------+--------------------------+
+## |            TOPIC Name Length LSB (VALUE = 4)        |   3
+## +-----------------------------------------------------+
+##
+##                          TOPIC
+##
+## Payload: Set these optionals
+## +--------------------------+--------------------------+
+## |                 Packet Identifier MSB               |   n
+## +-----------------------------------------------------+
+## +--------------------------+--------------------------+
+## |                 Packet Identifier LSB               |   n + 1
+## +-----------------------------------------------------+
+##
+## Application payload
+#
+struct Publish < Control
     @qos: QoS
     @dup: Bool
     @retain: Bool
@@ -178,17 +236,59 @@ struct Publish
     @pkid: Pkid
     @payload: Bytes
 
-    def initialize(@topic, @qos, @payload)
-        @dup = false
-        @retain = false
-        @pkid = Pkid.new(0_u16)
+    def initialize(@topic, @qos, @payload, @pkid, @retain = false, @dup = false)
+    end
+
+    def to_io(io : IO, format : IO::ByteFormat = IO::ByteFormat::SystemEndian)
+      remaining_len = 2 + @topic.bytesize + @payload.bytesize
+
+      if @qos != QoS::AtmostOnce
+        remaining_len += 2
+      end
+
+      io.write_byte(0x30_u8 | (@retain? 1_u8:0_u8) | @qos.value << 1 | (@dup? 1_u8:0_u8) << 3)
+      write_remaining_length(io, remaining_len)
+      write_mqtt_string(io, @topic)
+
+      if @qos != QoS::AtmostOnce
+        io.write_byte((@pkid.value >> 8).to_u8)
+        io.write_byte(@pkid.value.to_u8)
+      end
+
+      io.write(@payload)
     end
 end
 
+
+## Fixed header for PUBACK/PUBREC/PUBCOMP/PUBREL PACKET
+##
+## 7                          3                          0
+## +--------------------------+--------------------------+
+## |         ACK (1) NIBBLE   |     RESERVED             |   0
+## +--------------------------+--------------------------+
+## |    Remaining Len = Len of Varable header(2)         |   1
+## +-----------------------------------------------------+
+## 
+## Variable header ( LENGTH = 2 Bytes)
+##
+## +--------------------------+--------------------------+
+## |                 Packet Identifier MSB               |   2
+## +-----------------------------------------------------+
+## +--------------------------+--------------------------+
+## |                 Packet Identifier LSB               |   3
+## +-----------------------------------------------------+
+#
 struct Puback
     @pkid: Pkid
 
     def initialize(@pkid)
+    end
+
+    def to_io(io : IO, format : IO::ByteFormat = IO::ByteFormat::SystemEndian)
+      frame = UInt8.slice(0x40, 0x02)
+      io.write(frame)
+      io.write_byte((@pkid.value >> 8).to_u8)
+      io.write_byte(@pkid.value.to_u8)
     end
 end
 
@@ -197,6 +297,13 @@ struct Pubrec
 
     def initialize(@pkid)
     end
+
+    def to_io(io : IO, format : IO::ByteFormat = IO::ByteFormat::SystemEndian)
+      frame = UInt8.slice(0x50, 0x02)
+      io.write(frame)
+      io.write_byte((@pkid.value >> 8).to_u8)
+      io.write_byte(@pkid.value.to_u8)
+    end
 end
 
 struct Pubrel
@@ -204,12 +311,26 @@ struct Pubrel
 
     def initialize(@pkid)
     end
+
+    def to_io(io : IO, format : IO::ByteFormat = IO::ByteFormat::SystemEndian)
+      frame = UInt8.slice(0x62, 0x02)
+      io.write(frame)
+      io.write_byte((@pkid.value >> 8).to_u8)
+      io.write_byte(@pkid.value.to_u8)
+    end
 end
 
 struct Pubcomp
     @pkid: Pkid
 
     def initialize(@pkid)
+    end
+
+    def to_io(io : IO, format : IO::ByteFormat = IO::ByteFormat::SystemEndian)
+      frame = UInt8.slice(0x70, 0x02)
+      io.write(frame)
+      io.write_byte((@pkid.value >> 8).to_u8)
+      io.write_byte(@pkid.value.to_u8)
     end
 end
 
@@ -223,13 +344,53 @@ struct Subscribe
     end
 end
 
+
+## Fixed header for PUBLISH PACKET
+##
+## 7                          3                          0
+## +--------------------------+--------------------------+
+## |     PUBLISH (3) NIBBLE   | DUP(1), QoS(2), Retain(1)|   0
+## +--------------------------+--------------------------+
+## | Remaining Len = Len of Variable header(10) + Payload|   1
+## +-----------------------------------------------------+
+## 
+##
+## Variable header ( LENGTH = 2 Bytes)
+##
+## +--------------------------+--------------------------+
+## |                  Packet Identifier MSB              |   2
+## +-----------------------------------------------------+
+## +--------------------------+--------------------------+
+## |                  Packet Identifier MSB              |   3
+## +-----------------------------------------------------+
+##
+##
+## PAYLOAD (RETURN CODES)
+## +--------------------------+--------------------------+
+## |                 PAYLOAD BYTE 0 FOR SUB 0            |   n
+## +-----------------------------------------------------+
+## +--------------------------+--------------------------+
+## |                 PAYLOAD BYTE 1 FOR SUB 1            |   n + 1
+## +-----------------------------------------------------+
+##                          ....
+##                          ....
+## +--------------------------+--------------------------+
+## |                 PAYLOAD BYTE N FOR SUB N            |   n + N
+## +-----------------------------------------------------+
+
 struct Suback
     @pkid : Pkid
-    @return_code : UInt8
+    @return_codes : Array(UInt8)
 
-    def initialize
-        @pkid = 0_u8
-        @return_code = 0_u8
+    def initialize(@pkid, @return_codes)
+    end
+
+    def to_io(io : IO, format : IO::ByteFormat = IO::ByteFormat::SystemEndian)
+      remaining_len = 2 + @return_code.len
+      io.write_byte(0x90)
+      write_mqtt_string(io, remaining_len)
+
+      # TODO: Finish this
     end
 end
 
@@ -247,16 +408,14 @@ struct Unsuback
 
     def initialize(@pkid)
     end
-end
 
-require "socket"
-socket = TCPSocket.new("localhost", 1883)
-#socket = IO::Memory.new
-connect = Connect.new("hello", 10_u16)
-puts connect
-socket.write_bytes(connect, IO::ByteFormat::NetworkEndian)
-sleep 5
-# puts connect
+    def to_io(io : IO, format : IO::ByteFormat = IO::ByteFormat::SystemEndian)
+      frame = UInt8.slice(0x40, 0x02)
+      io.write(frame)
+      io.write_byte((@pkid.value >> 8).to_u8)
+      io.write_byte(@pkid.value.to_u8)
+    end
+end
 
 # connack = Connack.new
 # puts connack
